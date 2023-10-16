0c4f8ec3ceadbb7a1bd65b611e4eb3ece09e906b
TatjanaAlexeenko <127373243+TatjanaAlexeenko@users.noreply.github.com>
2023-10-16 16:27:16 +0700

    add __ to table_name

--- models/1_normalize/normalized_sheets_mytarget_banners_statjstics.sql
+++ models/1_normalize/normalized_sheets_mytarget_banners_statjstics.sql
@@ -6,9 +6,9 @@ JSONExtractString(`_airbyte_data`,'goals') as goals,
 JSONExtractString(`_airbyte_data`,'spent') as spent,
 JSONExtractString(`_airbyte_data`,'_banner_id') as banner_id,
 JSONExtractString(`_airbyte_data`,'_airbyte_ab_id') as airbyte_ab_id,
-toLowCardinality(table_name) as table_name,
+toLowCardinality(__table_name) as __table_name,
 toDateTime32(_airbyte_emitted_at) as _emitted_at,
 now() as _normalized_at 
 from
-(select *,'travelata._airbyte_raw_sheets_mytarget_travelata_normalize_mt_banners_statistics' as table_name
+(select *,'travelata._airbyte_raw_sheets_mytarget_travelata_normalize_mt_banners_statistics' as __table_name
 from travelata.`_airbyte_raw_sheets_mytarget_travelata_normalize_mt_banners_statistics`)
