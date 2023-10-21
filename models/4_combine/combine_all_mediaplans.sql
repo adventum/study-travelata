1fed9c8122cf852f7a3b920737977ac2d67fe48b
Aleksandra Anisimova <116505736+AleCity09@users.noreply.github.com>
2023-10-21 21:53:36 +0300

    Add files via upload

---
+++ models/4_combine/combine_all_mediaplans.sql
@@ -0,0 +1,29 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+with unnest_dates as (
+
+select * ,
+        dateAdd(planCostDateStart, arrayJoin(range(0, 1 + toUInt8(date_diff('day', planCostDateStart, planCostDateEnd))))) as planCostDate,
+        count(*) over(partition by productName, adSourceDirty, planCostDateStart, utmCampaign) as divide_by_days
+from {{ ref('join_all_mediaplans')}}        
+)
+
+select
+planCostDate,
+accountName,
+productName,
+cityCode,
+adSourceDirty,
+utmSource,
+utmMedium,
+utmCampaign,
+utmTerm,
+utmContent,
+planCost/divide_by_days as planCost,
+load_date
+
+from unnest_dates
