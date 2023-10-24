dc13ddefc7b10316520ec0437785c06bd5fb18ac
TatjanaAlexeenko <127373243+TatjanaAlexeenko@users.noreply.github.com>
2023-10-24 09:34:23 +0700

    add load_date to my_target and ym_visits

--- models/1_normalize/normalized_sheets_mytarget_banners_statjstics.sql
+++ models/1_normalize/normalized_sheets_mytarget_banners_statjstics.sql
@@ -8,7 +8,8 @@ JSONExtractString(`_airbyte_data`,'banner_id') as banner_id,
 JSONExtractString(`_airbyte_data`,'_airbyte_ab_id') as airbyte_ab_id,
 toLowCardinality(__table_name) as __table_name,
 toDateTime32(_airbyte_emitted_at) as _emitted_at,
-now() as _normalized_at 
+now() as _normalized_at,
+JSONExtractString(`_airbyte_data`,'load_date') as load_date
 from
 (select *,'travelata._airbyte_raw_sheets_mytarget_travelata_normalize_mt_banners_statistics' as __table_name
 from travelata.`_airbyte_raw_sheets_mytarget_travelata_normalize_mt_banners_statistics`)
