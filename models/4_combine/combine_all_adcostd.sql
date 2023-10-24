91ed841d7b04cc6c93a50921a113a35de339d8f3
Aleksandra Anisimova <city09@yandex.ru>
2023-10-24 23:04:34 +0300

    modified:   models/4_combine/combine_all_adcostd.sql

--- models/4_combine/combine_all_adcostd.sql
+++ models/4_combine/combine_all_adcostd.sql
@@ -12,8 +12,6 @@ union all
 select * replace (adCost * 1.2 as adCost) from {{ref('join_yd_all_adcost')}}
 union all
 select * from {{ref('join_yandexdisk_all_adcost')}}
-union all
-select * replace (adCost * 1.2 as adCost) from {{ref('join_vk_ads_all_adcost')}}
 )
 
 select * from union_all_adcost
