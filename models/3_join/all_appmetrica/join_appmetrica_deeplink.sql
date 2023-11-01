d26f509e931db3dcda762a37935a048a181e0a56
piettari <p.kechekyan@yandex.ru>
2023-11-01 14:31:38 +0500

    fix join_appmetrica

--- models/3_join/all_appmetrica/join_appmetrica_deeplink.sql
+++ models/3_join/all_appmetrica/join_appmetrica_deeplink.sql
@@ -16,7 +16,5 @@ city as cityName,
 assumeNotNull(COALESCE(NULLIF(google_aid, ''), NULLIF(ios_ifa, ''), appmetrica_device_id, '')) as advertisingId ,
 os_name as osName,
 publisher_name as publisherName,
-__table_name as tableName,
-from
-{{ref ('incremental_sheets_appmetrica_deeplinks')}}
-
+__table_name as tableName
+from {{ref ('incremental_sheets_appmetrica_deeplinks')}}
