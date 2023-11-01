ed801c7e1a454e1d1c8183c0394da6646b31fdcd
dmakhazen <d.makhazen@gmail.com>
2023-11-01 23:59:08 +0700

    обновил структуру репозитория

--- models/2_incremental/incremental_sheets_yandexdisk_mediaplans.sql
+++
@@ -1,9 +0,0 @@
-{{
-    config(
-        materialized='table',
-        order_by=('__table_name')
-    )
-}}
-
-SELECT *  REPLACE(parseDateTime(load_date, '%d.%m.%Y %H:%i:%S') as load_date)
-FROM {{ ref('normalized_sheets_yandexdisk_mediaplans')}}
