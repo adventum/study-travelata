3a89ce6d0446b609c5a42d449f49d5a8b8964cd6
pclv-ktrn <poceluevaekaterina@yandex.ru>
2023-11-07 12:39:12 +0300

    Добавила макрос metadata_ekaterina

---
+++ macros/meta_personal/metadata_ekaterina.sql
@@ -0,0 +1,53 @@
+{%- macro metadata_ekaterina() -%}
+entities:
+     Account:
+    keys:
+    - name: __table_name
+  Product:
+    keys:
+    - name: productName
+  AdSource:
+    keys:
+    - name: adSourceDirty
+  UtmParams:
+    keys:
+    - name: utmSource
+    - name: utmMedium
+    - name: utmCampaign
+    - name: utmTerm
+    - name: utmContent
+  UtmHash:
+    keys:
+    - name: utmHash
+  AdPhrase:
+    keys:
+    - name: adPhraseId
+  AdCampaign:
+    keys:
+    - name: adCampaignName
+    - name: adCampaignId
+  AdGroup:
+    keys:
+    - name: adGroupName
+    - name: adGroupId
+  Ad:
+    keys:
+    - name: adId
+  CityCode:
+    keys:
+    - name: cityCode
+links:
+  AdCostStat:
+    keys:
+    - name: toDate(__datetime)
+    - name: reportType
+    entities:
+    - Account
+    - AdSource
+    - AdCampaign
+    - AdGroup
+    - Ad
+    - AdPhrase
+    - UtmParams
+    - UtmHash
+{%- endmacro -%}
