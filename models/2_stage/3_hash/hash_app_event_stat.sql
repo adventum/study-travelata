ea15eaa16ca7ad380d26b3ac6110aef905461bd6
dmakhazen <d.makhazen@gmail.com>
2023-11-10 16:26:35 +0700

    добавил хэш и линк для апметрики

--- models/2_stage/3_hash/hash_app_event_stat.sql
+++ models/2_stage/3_hash/hash_app_event_stat.sql
@@ -1,7 +1,7 @@
 {{
     config(
         materialized='table',
-        order_by=('toDate(installDateTime)','accountName'),
+        order_by=('toDate(__datetime)'),
         enabled=true,
     )
 }}
@@ -9,8 +9,8 @@
 {%- set metadata = fromyaml(metadata_dmitrii()) -%}
 
 select *,
-    {{ link_hash('AppInstallStat', metadata) }},
-    {{ entity_hash('UtmParams', metadata) }},
+    {{ link_hash('AppEventStat', metadata) }},
+    {{ entity_hash('Account', metadata) }},
     {{ entity_hash('AppMetricaDeviceId', metadata) }},
     {{ entity_hash('MobileAdsId', metadata) }},
     {{ entity_hash('CrmUser', metadata) }},
