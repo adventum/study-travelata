979c9ec4b1038c592931f4788597d4cd007bafdc
piettari <p.kechekyan@yandex.ru>
2023-11-17 23:18:24 +0500

    add hash-link for session stat

--- models/2_stage/1_join/all_appmetrica/join_appmetrica_sessionstarts.sql
+++ models/2_stage/1_join/all_appmetrica/join_appmetrica_sessionstarts.sql
@@ -12,6 +12,7 @@ city AS cityName,
 assumeNotNull(COALESCE(NULLIF(google_aid, ''), NULLIF(ios_ifa, ''), appmetrica_device_id, '')) AS mobileAdsId,
 os_name AS osName,
 profile_id AS crmUserId,
-installation_id AS installationDeviceId,
+installation_id AS installationId,
+concat (installation_id, '-', appmetrica_device_id ) as installationDeviceId,
 __table_name
 FROM {{ref ('incremental_sheets_appmetrica_sessionstart')}}
