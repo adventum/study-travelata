fb5edad47d43e4bb1d81be5009eceeb17206d04e
dmakhazen <d.makhazen@gmail.com>
2023-11-21 21:20:56 +0700

    add events to appmetrica events

--- models/2_stage/1_join/all_appmetrica/join_appmetrica_events.sql
+++ models/2_stage/1_join/all_appmetrica/join_appmetrica_events.sql
@@ -14,9 +14,15 @@ select
     os_name as osName,
     city as cityName,
     session_id as sessionId,
+    event_name as eventName,
     JSONExtractString(event_json, 'item_name') AS itemName,
     JSONExtractString(event_json, 'item_id') AS itemId,
-    JSONExtractString(event_json, 'item_category') AS itemCategory
+    JSONExtractString(event_json, 'item_category') AS itemCategory,
+    if(event_name='serp', 1, 0) as serp,
+    if(event_name='hotel_page', 1, 0) as hotelPage,
+    if(event_name='tour_page', 1, 0) as tourPage,
+    if(event_name='chekout', 1, 0) as checkout,
+    if(event_name='payment', 1, 0) as payment
     
 
 from {{ ref('incremental_sheets_appmetrica_events') }}
