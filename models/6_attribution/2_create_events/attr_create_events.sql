50bec490026d970d9e65c7d8734b317676914620
TatjanaAlexeenko <127373243+TatjanaAlexeenko@users.noreply.github.com>
2023-11-22 11:41:38 +0700

    скорректировала attr_create_events

--- models/6_attribution/2_create_events/attr_create_events.sql
+++ models/6_attribution/2_create_events/attr_create_events.sql
@@ -2,7 +2,7 @@
     config(
         materialized='table',
         order_by=('qid', '__datetime', 'record_source', '__id'),
-        enabled=false
+        enabled=true
     )
 }}
 
@@ -13,18 +13,11 @@ select
         when record_source = 'full_link_app_install_stat' then 1
         when record_source = 'full_link_app_session_stat' then 1
         when record_source = 'full_link_app_deeplink_stat' then 1
-        when record_source = 'full_link_app_event_stat' and addToCartSessions = 1 then 2
-        when record_source = 'full_link_app_event_stat' and cartViewSessions = 1 then 2
-        when record_source = 'full_link_app_event_stat' and checkoutSessions = 1 then 3
-        when record_source = 'full_link_app_event_stat' and webSalesSessions = 1 then 4
-        when record_source = 'full_link_app_event_stat' and sales = 1 then 4
-        when record_source = 'full_link_app_event_stat' and registrationCardSessions = 1 then 2
-        when record_source = 'full_link_app_event_stat' and linkingCardToPhoneNumberSessions = 1 then 2
-        when record_source = 'full_link_app_event_stat' and registrationCashbackSessions = 1 then 2
-        when record_source = 'full_link_app_event_stat' and instantDiscountActivationSessions = 1 then 2
-        when record_source = 'full_link_app_event_stat' and couponActivationSessions = 1 then 2
-        when record_source = 'full_link_app_event_stat' and participationInLotterySessions = 1 then 2
-        when record_source = 'full_link_app_event_stat' and registrationButtonClick = 1 then 2
+        when record_source = 'full_link_app_event_stat' and serp = 1 then 2
+        when record_source = 'full_link_app_event_stat' and hotelPage = 1 then 3
+        when record_source = 'full_link_app_event_stat' and tourPage = 1 then 4
+        when record_source = 'full_link_app_event_stat' and checkout = 1 then 5
+        when record_source = 'full_link_app_event_stat' and payment = 1 then 6
         else 1
     end as __priority,
     __id,
@@ -39,8 +32,7 @@ select
     __datetime
 from {{ ref('attr_prepare_with_qid') }}
 where record_source = 'full_link_visit_stat'
-      and (addToCartSessions = 1 or cartViewSessions = 1 or registrationCardSessions = 1 or linkingCardToPhoneNumberSessions = 1 or linkingCardToPhoneNumberSessions = 1
-            or registrationCashbackSessions = 1 or instantDiscountActivationSessions = 1 or couponActivationSessions = 1 or participationInLotterySessions = 1)
+      and serp = 1
     union all
 select
     qid, record_source,
@@ -48,7 +40,8 @@ select
     __id,
     __datetime
 from {{ ref('attr_prepare_with_qid') }}
-where record_source = 'full_link_visit_stat' and checkoutSessions = 1
+where record_source = 'full_link_visit_stat'
+      and hotelPage = 1
     union all
 select
     qid, record_source,
@@ -56,4 +49,20 @@ select
     __id,
     __datetime
 from {{ ref('attr_prepare_with_qid') }}
-where record_source = 'full_link_visit_stat' and (webSalesSessions = 1 or sales = 1)
+where record_source = 'full_link_visit_stat' and tourPage = 1
+    union all
+select
+    qid, record_source,
+    5 as __priority,
+    __id,
+    __datetime
+from {{ ref('attr_prepare_with_qid') }}
+where record_source = 'full_link_visit_stat' and checkout = 1
+    union all
+select
+    qid, record_source,
+    6 as __priority,
+    __id,
+    __datetime
+from {{ ref('attr_prepare_with_qid') }}
+where record_source = 'full_link_visit_stat' and payment = 1
